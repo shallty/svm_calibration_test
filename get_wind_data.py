4279d7da3357ea92797a3b080572e1efd6def6f4
shallty <hulinfeng.ht@foxmail.com>
2018-01-13 23:06:58 +0800

    构造万得数据接口处理
    构造万得期权价格及到期日数据处理函数，直接返回期权代码及开始日当日和结束日当日期权价格信息

---
+++ get_wind_data.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 12 14:35:07 2018
+
+@author: H.T
+"""
+
+#
+# 万得数据接口数据处理
+# get_wind_data.py
+#
+import pandas as pd
+
+from WindPy import *
+from datetime import datetime as dt
+
+def get_wind_optiondata(startdate, enddate):
+    ''' 利用Wind数据接口获取期权交易市场数据。
+    
+    Parameters
+    ==========
+    startdate : str
+        行情数据开始日期
+    enddate : str
+        行情数据结束日期
+    
+    Results
+    =======
+    option_data_set : DataFrame
+        包含收盘价及到期日等信息的期权数据集
+    '''
+    w.start()
+    getstr1 = "startdate=" + startdate + ";enddate=" + enddate + ";exchange=sse;windcode=510050.SH;field=date,option_code,option_name,close"
+    getstr2 = "exchange=sse;windcode=510050.SH;status=all;field=wind_code,sec_name,exercise_price,listed_date,expire_date"
+    odo1 = w.wset("optiondailyquotationstastics", getstr1)
+    odo2 = w.wset("optioncontractbasicinfo", getstr2)
+    option_data = pd.DataFrame()
+    contract_data = pd.DataFrame()
+    for i in range(len(odo1.Fields)):
+        option_data[odo1.Fields[i]] = odo1.Data[i]
+    option_data.index = odo1.Data[0]
+    
+    for i in range(len(odo2.Fields)):
+        contract_data[odo2.Fields[i]] = odo2.Data[i]
+    
+    temp1 = option_data[option_data.index == dt.strptime(startdate, '%Y-%m-%d')]
+    temp2 = option_data[option_data.index == dt.strptime(enddate, '%Y-%m-%d')]
+    ops = temp1.append(temp2, ignore_index=True)
+    contract_data.rename(columns={contract_data.columns[0] : 'option_code'}, 
+                                  inplace=True)
+    option_data_set = ops.merge(contract_data, on='option_code')
+    return option_data_set
